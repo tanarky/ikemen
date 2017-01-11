a4ac6ef111e7fc0081d4ff0fdc86ce1f314e37ae
Satoshi Tanaka <tanarky@gmail.com>
2017-01-11 09:15:33 +0900

    fixed game page design

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,5 +8,6 @@ class GamesController < ApplicationController
   private
   def set_game
     @game = Game.find(params[:id])
+    @T = {icon: "//is4.mzstatic.com/image/thumb/Purple111/v4/53/c0/70/53c070cc-a7a6-95ca-0a1a-6b132d89d4f1/source/175x175bb.jpg"}
   end
 end
